c257ed5fe23272a62f5d397352697c2119631610
Cefn Hoile <git@cefn.com>
2015-04-15 14:07:16 +0100

    Moved Unary Alarm

--- sketchbook/shrimpingit/alarmclock/Clock05UnaryAlarm/Clock05UnaryAlarm.ino
+++ sketchbook/shrimpingit/alarmclock/Clock05UnaryAlarm/Clock05UnaryAlarm.ino
@@ -1,9 +1,13 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include <rtttl.h>
-#include <Narcoleptic.h>
 
 RTC_DS1307 rtc;
+
+#define COMMANDMAX 19
+String command = "";
+String field = "";
+int fieldStart = 0;
 
 //See more RTTTL songs at https://github.com/cefn/non-blocking-rtttl-arduino/blob/master/rtttl/examples/ProgmemSong/ProgmemSong.ino
 //or look online for your favourite chimes, being careful to remove unnecessary spaces
@@ -50,66 +54,152 @@ int brightness = 255;
 int alarmHour = 7;
 int alarmMinute = 00;
 
-boolean report = true;
+boolean report = false;
 
-void setup(void)
-{
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-
-  Serial.begin(57600);
+void setup(){
+  //start all utility libraries
+  Serial.begin(9600);
   Wire.begin();
   rtc.begin();
+
+  //reserve enough string memory for ISO8601 date string like 2015-06-01T12:00:00
+  command.reserve(COMMANDMAX + 1); 
+  field.reserve(COMMANDMAX + 1);
+
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
  
   player.setSong(song_P);
   
-  
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
-    
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    //rtc.adjust(DateTime(2015,2, 23, 1, 0, 0));
-  }
+  }  
   
-  DateTime now = rtc.now();
-  
-  //for testing
-  //alarmHour = now.hour();
-  //alarmMinute = now.minute() + 2;
 }
 
-void loop(void)
-{
-    
-  DateTime now;
+void loop(){
 
-  now = rtc.now();
+  while(Serial.available()){
+    char nextChar = Serial.read();
+    if(nextChar != '\n'){
+      command += nextChar;
+    }
+    else{
+      processCommand();
+      command.remove(0);
+    }
+  }
+    
+  DateTime now = rtc.now();
   
-  setTime(now.hour(), now.minute());
+  showTime(now.hour(), now.minute());
   
-  while(now.hour() == alarmHour && now.minute() == alarmMinute){
+  //tune is re-triggered throughout the alarm minute
+  while(now.hour() == alarmHour && now.minute() == alarmMinute){ 
     player.finishSong();
     now = rtc.now();
   }
-
-  Narcoleptic.delay(10000);
-  
-  if(report){
-    Serial.print(now.hour());
-    Serial.print(":");
-    Serial.print(now.minute());
-    Serial.print(":");
-    Serial.print(now.second());
-    Serial.println();
-    Serial.flush();
-  }
-  
+    
 }
 
-void setTime(int newHours, int newMins){ 
+void processCommand(){
+  
+  int year,month,day,hour,minute,second;
+     
+  fieldStart = 0;
+  
+  //try to parse an ISO 8601 date fragment
+   
+  year = nextIntField('-');
+  if(year != -1){
+    month = nextIntField('-');
+    day = nextIntField('T');
+    if(day == -1){
+      day = remainingIntField();
+    }      
+    setDate(year, month, day);
+  }
+
+  //try to parse an ISO 8601 time fragment
+
+  hour = nextIntField(':');
+  if(hour != -1){
+    minute = nextIntField(':');
+    if(minute == -1){
+      minute = remainingIntField();
+      second = 0;
+    }
+    else{
+      second = remainingIntField();      
+    }
+    setTime(hour, minute, second);
+  }
+  
+  //always print out the date and time (sending just newline reports time)
+  printTime();
+
+  command.remove(0);
+
+}
+
+int nextIntField(char terminator){
+  int fieldEnd = command.indexOf(terminator, fieldStart);
+  if(fieldEnd != -1){
+    field += command.substring(fieldStart,fieldEnd);
+    fieldStart = fieldEnd + 1;
+    int value = field.toInt();
+    field.remove(0);
+    return value;
+  }
+  else{
+    return -1;
+  }
+}
+
+int remainingIntField(){
+  field += command.substring(fieldStart);
+  int value = field.toInt();
+  field.remove(0);
+  return value;
+}
+
+
+/** Sets both the date and the time */
+void setDateTime(  uint16_t year, uint8_t  month, uint8_t  day, uint8_t  hour, uint8_t  minute, uint8_t  second){
+  rtc.adjust(DateTime(year, month, day, hour, minute, second));
+}
+
+/** Sets only the date */
+void setDate(  uint16_t year, uint8_t  month, uint8_t  day){ //changes only the date
+  DateTime now = rtc.now();
+  setDateTime(year, month, day, now.hour(), now.minute(), now.second());
+}
+
+/** Sets only the time */
+void setTime(uint8_t  hour, uint8_t  minute, uint8_t  second){ //changes only the time
+  DateTime now = rtc.now();
+  setDateTime(now.year(), now.month(), now.day(), hour, minute, second);
+}
+
+void printTime(){
+    DateTime now = rtc.now();    
+    Serial.print(now.year(), DEC);
+    Serial.print('-');
+    Serial.print(now.month(), DEC);
+    Serial.print('-');
+    Serial.print(now.day(), DEC);
+    Serial.print('T');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+}
+
+
+
+void showTime(int newHours, int newMins){ 
   //calculate for display
   newHours = newHours % 12; //bounds checking
   newHours = newHours == 0? 12: newHours; //make 0 to be 12
