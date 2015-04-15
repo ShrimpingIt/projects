2c0dcbb79d8657a81db04a32c0f6d767dfb8e28f
Cefn Hoile <git@cefn.com>
2015-04-15 21:26:01 +0100

    Last version based on WS2803 chip

---
+++ sketchbook/shrimpingit/alarmclock/Clock05UnaryAlarmWS2803/Clock05UnaryAlarmWS2803.ino
@@ -0,0 +1,278 @@
+#include <Wire.h>
+#include "RTClib.h"
+#include <rtttl.h>
+
+RTC_DS1307 rtc;
+
+#define COMMANDMAX 19
+String command = "";
+String field = "";
+int fieldStart = 0;
+
+//See more RTTTL songs at https://github.com/cefn/non-blocking-rtttl-arduino/blob/master/rtttl/examples/ProgmemSong/ProgmemSong.ino
+//or look online for your favourite chimes, being careful to remove unnecessary spaces
+const char song_P[] PROGMEM = "Wham- Wake Me Up:d=4,o=5,b=300:f#5,2a5,2b5.,p,f#5,2a5,b5,2f#5,2d5,p,d5,e5,2f#5,2g5,f#5,e5,d5,2f#5,a5,2f#5,d5,2p";
+
+ProgmemPlayer player(9);
+
+boolean lit[36];
+const int clockPin = A3;
+const int dataPin = A2;
+
+//Each of these binary sequences should have 
+//an easily distinguishable number of LED lights
+int distinctNumbers[] = {
+  0b000000000000, //0
+  0b000000100000, //1
+  0b100000000001, //2
+  0b100010001000, //3
+  0b001001001001, //4
+  0b011000100011, //5
+  0b101010101010, //6
+  0b100000111111, //7
+  0b011011011011, //8
+  0b011101110111, //9
+  0b101111111101, //10
+  0b111111011111, //11
+  0b111111111111  //12
+};
+
+//these are the assignments of each LED driver
+//pin to an hour or minute light
+//positive number from 1 to 12 is an hour indicator light
+//negative number from -1 to -12 is a minutes indicator light
+//zero means do not use
+int assignments[] ={
+    1,  2, 0,  3, 0,  4, 0,  5,  6,
+   -6, -5, 0, -4, 0, -3, 0, -2, -1,
+  -12,-11, 0,-10, 0, -9, 0, -8, -7,
+    7,  8, 0,  9, 0, 10, 0, 11, 12
+};
+
+int brightness = 255;
+
+int alarmHour = 7;
+int alarmMinute = 00;
+
+boolean report = false;
+
+void setup(){
+  //start all utility libraries
+  Serial.begin(9600);
+  Wire.begin();
+  rtc.begin();
+
+  //reserve enough string memory for ISO8601 date string like 2015-06-01T12:00:00
+  command.reserve(COMMANDMAX + 1); 
+  field.reserve(COMMANDMAX + 1);
+
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+ 
+  player.setSong(song_P);
+  
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+  }  
+  
+}
+
+void loop(){
+
+  while(Serial.available()){
+    char nextChar = Serial.read();
+    if(nextChar != '\n'){
+      command += nextChar;
+    }
+    else{
+      processCommand();
+      command.remove(0);
+    }
+  }
+    
+  DateTime now = rtc.now();
+  
+  showTime(now.hour(), now.minute());
+  
+  //tune is re-triggered throughout the alarm minute
+  while(now.hour() == alarmHour && now.minute() == alarmMinute){ 
+    player.finishSong();
+    now = rtc.now();
+  }
+    
+}
+
+void processCommand(){
+  
+  int year,month,day,hour,minute,second;
+     
+  fieldStart = 0;
+  
+  //try to parse an ISO 8601 date fragment
+   
+  year = nextIntField('-');
+  if(year != -1){
+    month = nextIntField('-');
+    day = nextIntField('T');
+    if(day == -1){
+      day = remainingIntField();
+    }      
+    setDate(year, month, day);
+  }
+
+  //try to parse an ISO 8601 time fragment
+
+  hour = nextIntField(':');
+  if(hour != -1){
+    minute = nextIntField(':');
+    if(minute == -1){
+      minute = remainingIntField();
+      second = 0;
+    }
+    else{
+      second = remainingIntField();      
+    }
+    setTime(hour, minute, second);
+  }
+  
+  //always print out the date and time (sending just newline reports time)
+  printTime();
+
+  command.remove(0);
+
+}
+
+int nextIntField(char terminator){
+  int fieldEnd = command.indexOf(terminator, fieldStart);
+  if(fieldEnd != -1){
+    field += command.substring(fieldStart,fieldEnd);
+    fieldStart = fieldEnd + 1;
+    int value = field.toInt();
+    field.remove(0);
+    return value;
+  }
+  else{
+    return -1;
+  }
+}
+
+int remainingIntField(){
+  field += command.substring(fieldStart);
+  int value = field.toInt();
+  field.remove(0);
+  return value;
+}
+
+
+/** Sets both the date and the time */
+void setDateTime(  uint16_t year, uint8_t  month, uint8_t  day, uint8_t  hour, uint8_t  minute, uint8_t  second){
+  rtc.adjust(DateTime(year, month, day, hour, minute, second));
+}
+
+/** Sets only the date */
+void setDate(  uint16_t year, uint8_t  month, uint8_t  day){ //changes only the date
+  DateTime now = rtc.now();
+  setDateTime(year, month, day, now.hour(), now.minute(), now.second());
+}
+
+/** Sets only the time */
+void setTime(uint8_t  hour, uint8_t  minute, uint8_t  second){ //changes only the time
+  DateTime now = rtc.now();
+  setDateTime(now.year(), now.month(), now.day(), hour, minute, second);
+}
+
+void printTime(){
+    DateTime now = rtc.now();    
+    Serial.print(now.year(), DEC);
+    Serial.print('-');
+    Serial.print(now.month(), DEC);
+    Serial.print('-');
+    Serial.print(now.day(), DEC);
+    Serial.print('T');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+}
+
+
+
+void showTime(int newHours, int newMins){ 
+  //calculate for display
+  newHours = newHours % 12; //bounds checking
+  newHours = newHours == 0? 12: newHours; //make 0 to be 12
+  newMins %= 60; //bounds checking
+  newMins /= 5; //12 segments of 5 minuts each
+  //send calculated values
+  shiftLights(newHours, newMins);
+}
+
+/** Given a number to display
+* should the LED at a given position be lit? 
+* Uses a special distinctNumbers matrix to decide
+* (making numbers easier to distinguish visually)
+*/
+boolean distinctNumberIsLit(int numLights, int pos){
+  //inspects an individual bit to se if this hour LED should be lit
+  return (distinctNumbers[numLights] & (1 << pos)) != 0;  
+}
+
+boolean rightUnaryIsLit(int numLights, int pos){
+  return numLights > pos;
+}
+
+boolean leftUnaryIsLit(int numLights, int pos){
+  return numLights > (11 - pos);
+}
+
+boolean centerUnaryIsLit(int numLights, int pos){
+  return abs(pos - 6) <= (numLights / 2);
+}
+
+void shiftLights(int showHours, int showMinutes){
+    
+  int showLights; //count of lights illuminated in the current bank
+  int shiftPos; //the current pin which is being written to
+  
+  //go through all the pins of both shift registers
+  for(shiftPos = 0; shiftPos < 36; shiftPos++){
+    
+    //figure out the assigned role of this pin 
+    //value between 1 and 12 (magnitude), and if it's an hour or minute (sign)
+    int assignment = assignments[shiftPos];
+
+    if(assignment == 0){ //it's not meant to be active (has no LED in this pin)
+      lit[shiftPos] = false; //leave it off
+    }
+    else {
+      if(assignment >= 0){ //positive number indicates an hour light
+        assignment = assignment - 1;  //shift to index LEDs from 0 to 11, not 1 to 12
+        lit[shiftPos] = distinctNumberIsLit(showHours, assignment); 
+      }
+      else { //negative number indicates a minute light
+        assignment = abs(assignment) - 1; //change sign and shift to index LEDs from 0 to 11 not 1 to 12
+        lit[shiftPos] = leftUnaryIsLit(showMinutes, assignment); 
+      }
+    }    
+        
+    shiftOut(dataPin, clockPin, MSBFIRST, lit[shiftPos] ? brightness : 0); //set current light
+        
+  } 
+
+  digitalWrite(clockPin, LOW);
+  delay(1);
+ 
+  if(report){
+    Serial.print("arr:");
+    for(shiftPos = 0; shiftPos < 36; shiftPos++){
+      Serial.print(lit[shiftPos]?"1":"0");
+    }
+    Serial.println();
+    Serial.flush();
+  }
+
+
+}
+ 
